fe93c98eef8b6cf92600740d055aea1695c5c0ec
Haroldo Vélez <hvelez020@gmail.com>
2019-06-17 21:06:49 +0000

    [add] 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0
+ * selection_sort - sorts an array of integers in ascending order using the
+ * Selection sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ * Return: none
  */
-int main(void)
+void selection_sort(int *array, size_t size)
 {
-	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-	size_t n = sizeof(array) / sizeof(array[0]);
+	unsigned int i, j, sw;
+	int tmp, tmpInd;
 
-	print_array(array, n);
-	printf("\n");
-	selection_sort(array, n);
-	printf("\n");
-	print_array(array, n);
-	return (0);
+	for (i = 0; i < size; i++)
+	{
+		sw = 0;
+		tmp = array[i];
+		for (j = i; j < size; j++)
+		{
+			if (array[j] < tmp)
+			{
+				tmpInd = j;
+				tmp = array[j];
+				sw = 1;
+			}
+
+		}
+		if (sw == 1)
+		{
+			array[tmpInd] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+		}
+	}
 }
